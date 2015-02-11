d0da6c7a6c44dd390ace1a78422cdb868f205a72
Evgeny Orlov <eugene.a.orlov@gmail.com>
2015-02-11 23:06:03 +0300

    20150211

--- TODO-eam-momentum.R
+++ TODO-eam-momentum.R
@@ -1,9 +1,9 @@
-# http://www.capitalspectator.com/ A Momentum-Based Trading Signal With Strategic
-# Value
+# http://www.capitalspectator.com/
+# A Momentum-Based Trading Signal With Strategic Value
 
 # load packages
 library("TTR")
-library("zoo")
+library("xts")
 library("quantmod")
 
 # download daily S&P 500 prices from Dec 31, 1990 forward
@@ -13,24 +13,32 @@ gspc <- getSymbols("^gspc", from = "1990-12-31", auto.assign = FALSE)
 eam <- function(x, y, z) {
   # x=ticker, y=lookback period for forecast, z=SMA period
   a <- na.omit(ROC(Cl(x), 1, "discrete"))
+  print(paste("a", length(a)))
   # forecast based on 'y' trailing period returns
   b <- na.omit(SMA(a, y))
+  print(paste("b", length(b)))
   # lag forecasts by 1 period
   c <- na.omit(Lag(b, k = 1))
+  print(paste("c", length(c)))
   # combine lagged forecasts with actual returns into one file
   d <- na.omit(cbind(c, a))
+  print(paste("d", length(d)))
   # actual daily return less forecast
   e <- as.xts(apply(d, 1, diff))
+  print(paste("e", length(e)))
   # mean absolute error
-  f <- to.daily(na.omit(rollapplyr(e, y, function(x) mean(abs(x)))), drop.time = TRUE, 
-    OHLC = FALSE)
+  f <- to.daily(na.omit(rollapplyr(e, y, function(x) mean(abs(x)))), 
+                drop.time = FALSE, OHLC = FALSE)
+  print(paste("f", length(f)))
   # combine actual return with MAE into one file
   g <- cbind(a, f)
+  print(paste("g", length(g)))
   # divide actual return by MAE
   h <- na.omit(a[, 1]/g[, 2])
+  print(paste("h", length(h)))
   # generate 200-day moving average of adjusted return
   i <- na.omit(SMA(h, z))
-  
+  print(paste("i", length(i)))
   # lag adjusted return signal by one day for trading analysis
   j <- na.omit(Lag(ifelse(i > 0, 1, 0)))
 }
@@ -39,53 +47,53 @@ eam.sig <- eam(gspc, 10, 200)
 
 # function to generate raw EAM signal data
 eam.ret <- function(x, y, z) {
-  # x=ticker, y=lookback period for vol forecast, z=SMA period
+  # x = ticker, y = lookback period for vol forecast, z = SMA period
   a <- eam(x, y, z)
-  print("a")
-  print(head(a))
-  print(length(a))
+#  print("a")
+#  print(head(a))
+#  print(length(a))
   b <- na.omit(ROC(Cl(x), 1, "discrete"))
-  print("b")
-  print(head(b))
-  print(length(b))
+#  print("b")
+#  print(head(b))
+#  print(length(b))
   c <- length(a) - 1
-  print("c")
-  print(head(c))
-  print(length(c))
+#  print("c")
+#  print(head(c))
+#  print(length(c))
   d <- tail(b, c)
-  print("d")
-  print(head(d))
-  print(length(d))
+#  print("d")
+#  print(head(d))
+#  print(length(d))
   e <- d * a
-  print("e")
-  print(head(e))
-  print(length(e))
+#  print("e")
+#  print(head(e))
+#  print(length(e))
   f <- cumprod(c(100, 1 + e))
-  print("f")
-  print(head(f))
-  print(length(f))
+#  print("f")
+#  print(head(f))
+#  print(length(f))
   g <- tail(b, c)
-  print("g")
-  print(head(g))
-  print(length(g))
+#  print("g")
+#  print(head(g))
+#  print(length(g))
   h <- cumprod(c(100, 1 + g))
-  print("h")
-  print(head(h))
-  print(length(h))
+#  print("h")
+#  print(head(h))
+#  print(length(h))
   i <- cbind(f, h)
   colnames(i) <- c("model", "asset")
-  print("i")
-  print(head(i))
-  print(length(i))
+#  print("i")
+#  print(head(i))
+#  print(length(i))
   date.a <- c((first(tail((as.Date(index(x))), c)) - 1), (tail((as.Date(index(x))), 
     c)))
-  print("date.a")
-  print(head(date.a))
-  print(length(date.a))
+#  print("date.a")
+#  print(head(date.a))
+#  print(length(date.a))
   j <- xts(i, date.a)
-  print("j")
-  print(head(j))
-  print(length(j))
+#  print("j")
+#  print(head(j))
+#  print(length(j))
   return(j)
 }
 
